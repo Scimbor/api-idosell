07bd28bc5e62c4c79f5beeb4379dca1ca272e76f
DESKTOP-I1PK30V\Marcin <marcin.scim@gmail.com>
2024-11-23 20:58:23 +0100

    Config file

--- src/IdosellServiceProvider.php
+++ src/IdosellServiceProvider.php
@@ -8,11 +8,13 @@ class IdosellServiceProvider extends ServiceProvider
 {
     public function register()
     {
-
+        $this->mergeConfigFrom(__DIR__.'/../config/idosell.php', 'idosell');
     }
 
     public function boot()
     {
-
+        $this->publishes([
+            __DIR__.'/../config/idosell.php' => config_path('idosell.php'),
+        ], 'config');
     }
 }
---
+++ src/config/idosell.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'api_key'=> env('IDOSELL_API_PASSWORD'),
+    'base_url'=> env('IDOSELL_API_BASE_URL', 'https://api.idosell.com'),
+];

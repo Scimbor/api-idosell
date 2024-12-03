3a5ed8596e77c88dccc7003ed22d760280a51743
DESKTOP-I1PK30V\Marcin <marcin.scim@gmail.com>
2024-12-03 22:54:25 +0100

    Added parameter support for get/post/put

--- src/Commands/TestApiIdosellPackage.php
+++ src/Commands/TestApiIdosellPackage.php
@@ -37,6 +37,16 @@ class TestApiIdosellPackage extends Command
             ],
         ])->post();
 
+        // $r = IdosellApi::request('returns/returns', [
+        //     'range' => [
+        //         'date' => [
+        //             'date_begin' => '2002-01-01',
+        //             'date_end' => date('Y-m-d'),
+        //             'dates_type' => 'date_end',
+        //         ],
+        //     ],
+        // ])->get();
+
         dump('Dane z zapytania');
         Storage::put('products.json', json_encode($r));
 
--- src/IdosellApiService.php
+++ src/IdosellApiService.php
@@ -8,6 +8,7 @@ use Exception;
 class IdosellApiService
 {
     private const API_ENDPOINT = '/api/admin/v3/';
+    private const DEFAULT_REQUEST_METHOD = 'get';
     private const ALLOWED_REQUEST_METHODS = [
         'post',
         'get',
@@ -43,8 +44,6 @@ class IdosellApiService
                 'timeout'  => 180.0,
                 'headers' => [
                     'X-API-KEY' => $this->config->api_key,
-                    'Accept' => 'application/json',
-                    'Content-Type' => 'application/json',
                 ],
             ]);
         }
@@ -58,12 +57,14 @@ class IdosellApiService
         return $this;
     }
 
-    public function __call($method, $args)
+    public function __call($method = self::DEFAULT_REQUEST_METHOD, $args)
     {
         // Sprawdzamy, czy metoda jest jedną z dozwolonych
         if (!in_array($method, self::ALLOWED_REQUEST_METHODS)) {
             throw new \BadMethodCallException("Metoda $method nie istnieje.");
         }
+
+        $this->params = (($method === self::DEFAULT_REQUEST_METHOD) ? ['query' => $this->params] : ['json' => $this->params]);
 
         $response = $this->client->request($method, $this->url, $this->params);
         $response = $response->getBody();
